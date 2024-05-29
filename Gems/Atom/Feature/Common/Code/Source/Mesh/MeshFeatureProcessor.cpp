o3de/o3de

Image-related resources
Co-authored-by: Martin Winter <martin.winter@huawei.com>
Co-authored-by: Joerg H. Mueller <joerg.mueller@huawei.com>
Signed-off-by: Martin Winter <martin.winter@huawei.com>

Gems/Atom/Feature/Common/Code/Source/Mesh/MeshFeatureProcessor.cpp
@@ -2337,7 +2337,7 @@ namespace AZ
                         if (image.get())
                         {
                             subMesh.m_textureFlags |= RayTracingSubMeshTextureFlags::BaseColor;
-                            subMesh.m_baseColorImageView = image->GetImageView();
+                            subMesh.m_baseColorImageView = image->GetImageView()->GetDeviceImageView(RHI::MultiDevice::DefaultDeviceIndex);
                             baseColorImage = image;
                         }
                     }
@@ -2349,7 +2349,7 @@ namespace AZ
                         if (image.get())
                         {
                             subMesh.m_textureFlags |= RayTracingSubMeshTextureFlags::Normal;
-                            subMesh.m_normalImageView = image->GetImageView();
+                            subMesh.m_normalImageView = image->GetImageView()->GetDeviceImageView(RHI::MultiDevice::DefaultDeviceIndex);
                         }
                     }
 
@@ -2360,7 +2360,7 @@ namespace AZ
                         if (image.get())
                         {
                             subMesh.m_textureFlags |= RayTracingSubMeshTextureFlags::Metallic;
-                            subMesh.m_metallicImageView = image->GetImageView();
+                            subMesh.m_metallicImageView = image->GetImageView()->GetDeviceImageView(RHI::MultiDevice::DefaultDeviceIndex);
                         }
                     }
 
@@ -2371,7 +2371,7 @@ namespace AZ
                         if (image.get())
                         {
                             subMesh.m_textureFlags |= RayTracingSubMeshTextureFlags::Roughness;
-                            subMesh.m_roughnessImageView = image->GetImageView();
+                            subMesh.m_roughnessImageView = image->GetImageView()->GetDeviceImageView(RHI::MultiDevice::DefaultDeviceIndex);
                         }
                     }
 
@@ -2382,7 +2382,7 @@ namespace AZ
                         if (image.get())
                         {
                             subMesh.m_textureFlags |= RayTracingSubMeshTextureFlags::Emissive;
-                            subMesh.m_emissiveImageView = image->GetImageView();
+                            subMesh.m_emissiveImageView = image->GetImageView()->GetDeviceImageView(RHI::MultiDevice::DefaultDeviceIndex);
                         }
                     }
 

accesspointmg/org.o3de.repo.atom

Gems/Atom/Feature/Common/Code/Source/Mesh/MeshFeatureProcessor.cpp
@@ -2403,7 +2403,7 @@ namespace AZ
                         if (image.get())
                         {
                             subMeshMaterial.m_textureFlags |= RayTracingSubMeshTextureFlags::BaseColor;
-                            subMeshMaterial.m_baseColorImageView = image->GetImageView();
+                            subMeshMaterial.m_baseColorImageView = image->GetImageView()->GetDeviceImageView(RHI::MultiDevice::DefaultDeviceIndex);
                             baseColorImage = image;
                         }
                     }
@@ -2415,7 +2415,7 @@ namespace AZ
                         if (image.get())
                         {
                             subMeshMaterial.m_textureFlags |= RayTracingSubMeshTextureFlags::Normal;
-                            subMeshMaterial.m_normalImageView = image->GetImageView();
+                            subMeshMaterial.m_normalImageView = image->GetImageView()->GetDeviceImageView(RHI::MultiDevice::DefaultDeviceIndex);
                         }
                     }
 
@@ -2426,7 +2426,7 @@ namespace AZ
                         if (image.get())
                         {
                             subMeshMaterial.m_textureFlags |= RayTracingSubMeshTextureFlags::Metallic;
-                            subMeshMaterial.m_metallicImageView = image->GetImageView();
+                            subMeshMaterial.m_metallicImageView = image->GetImageView()->GetDeviceImageView(RHI::MultiDevice::DefaultDeviceIndex);
                         }
                     }
 
@@ -2437,7 +2437,7 @@ namespace AZ
                         if (image.get())
                         {
                             subMeshMaterial.m_textureFlags |= RayTracingSubMeshTextureFlags::Roughness;
-                            subMeshMaterial.m_roughnessImageView = image->GetImageView();
+                            subMeshMaterial.m_roughnessImageView = image->GetImageView()->GetDeviceImageView(RHI::MultiDevice::DefaultDeviceIndex);
                         }
                     }
 
@@ -2448,7 +2448,7 @@ namespace AZ
                         if (image.get())
                         {
                             subMeshMaterial.m_textureFlags |= RayTracingSubMeshTextureFlags::Emissive;
-                            subMeshMaterial.m_emissiveImageView = image->GetImageView();
+                            subMeshMaterial.m_emissiveImageView = image->GetImageView()->GetDeviceImageView(RHI::MultiDevice::DefaultDeviceIndex);
                         }
                     }
 