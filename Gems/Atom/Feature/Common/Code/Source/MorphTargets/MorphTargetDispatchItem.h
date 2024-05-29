martinwinter-huawei/o3de

SRG-related resources.
Signed-off-by: Joerg H. Mueller <joerg.mueller@huawei.com>

Gems/Atom/Feature/Common/Code/Source/MorphTargets/MorphTargetDispatchItem.h
@@ -11,7 +11,7 @@
 #include <Atom/Feature/MorphTargets/MorphTargetInputBuffers.h>
 
 #include <Atom/RPI.Public/Shader/ShaderReloadNotificationBus.h>
-#include <Atom/RHI/SingleDeviceDispatchItem.h>
+#include <Atom/RHI/MultiDeviceDispatchItem.h>
 #include <Atom/RHI/ConstantsData.h>
 #include <AtomCore/Instance/Instance.h>
 
@@ -58,7 +58,7 @@ namespace AZ
 
             bool Init();
 
-            const RHI::SingleDeviceDispatchItem& GetRHIDispatchItem() const;
+            const AZ::RHI::MultiDeviceDispatchItem& GetRHIDispatchItem() const;
 
             void SetWeight(float weight);
             float GetWeight() const;
@@ -71,7 +71,7 @@ namespace AZ
             void OnShaderAssetReinitialized(const Data::Asset<RPI::ShaderAsset>& shaderAsset) override;
             void OnShaderVariantReinitialized(const RPI::ShaderVariant& shaderVariant) override;
 
-            RHI::SingleDeviceDispatchItem m_dispatchItem;
+            RHI::MultiDeviceDispatchItem m_dispatchItem;
 
             // The morph target shader used for this instance
             Data::Instance<RPI::Shader> m_morphTargetShader;

accesspointmg/org.o3de.repo.atom

Co-authored-by: Martin Winter <martin.winter@huawei.com>
Co-authored-by: Joerg H. Mueller <joerg.mueller@huawei.com>

Gems/Atom/Feature/Common/Code/Source/MorphTargets/MorphTargetDispatchItem.h
@@ -11,7 +11,7 @@
 #include <Atom/Feature/MorphTargets/MorphTargetInputBuffers.h>
 
 #include <Atom/RPI.Public/Shader/ShaderReloadNotificationBus.h>
-#include <Atom/RHI/SingleDeviceDispatchItem.h>
+#include <Atom/RHI/MultiDeviceDispatchItem.h>
 #include <Atom/RHI/ConstantsData.h>
 #include <AtomCore/Instance/Instance.h>
 
@@ -58,7 +58,7 @@ namespace AZ
 
             bool Init();
 
-            const RHI::SingleDeviceDispatchItem& GetRHIDispatchItem() const;
+            const AZ::RHI::SingleDeviceDispatchItem& GetRHIDispatchItem() const;
 
             void SetWeight(float weight);
             float GetWeight() const;